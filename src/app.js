2d99a76078e570a9ee8829ee3dc91fa96d92495e
WahajAhmedIqbal <wahaj465@gmail.com>
2025-10-15 23:43:19 +0500

    first-2

--- src/app.js
+++ src/app.js
@@ -3,18 +3,21 @@ import express from "express";
 import dotenv from "dotenv";
 import taskRoutes from "./routes/taskRoutes.js";
 
-
 dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: ["http://localhost:5173", "https://ai-assistant-frontend-zeta.vercel.app/"], // replace with your actual Vercel URL
+  origin: ["http://localhost:5173", "https://ai-assistant-frontend-zeta.vercel.app"], 
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
 
 app.use(express.json());
 app.use("/api", taskRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
